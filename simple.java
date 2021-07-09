d36855c468af43b3283187ac1a66d17587384def
API1286 <yasha.srinivas@apisero.com>
2021-07-09 09:37:34 +0530

    same local repo for both git & bb

--- simple.java
+++ simple.java
@@ -2,6 +2,8 @@ class simple{
     public static void main(String args[]){  
      System.out.println("Hello JAVA");
      System.out.println("Hello to git push");  
+
+	System.out.println("Hello to git push and bitbucket push from same local repo"); 
         
     }  
 }  
